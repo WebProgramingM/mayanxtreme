ef7cd133244ae8c91f64becee0f4321e0079a606
monzter50 <josemonzter50@gmail.com>
2018-02-17 18:05:52 -0500

    cambios secciones

---
+++ array-ingles.php
@@ -0,0 +1,116 @@
+<?php 
+$op = array(
+
+	'menuNav' => array(
+		['#', 'HOME'],
+		['#', 'TOURS'],
+		['#', 'GALLERY'],
+		['#', 'CONTACT'],
+	),
+	'footerNav' => array(
+		['http://grupoandersons.com/unete','Bolsa de Trabajo','bolsa.png'],
+		['aviso.php','Aviso de Privacidad','aviso.png'],
+		['http://andersons.mx/','Facturación','facturacion.png']
+
+	),
+
+);
+
+
+$services = array(
+
+	'action' => array(
+		[
+			'cont'  => array(
+
+				[
+					"title"=>"WAKEBOARDING",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>1
+
+				],
+				[
+					"title"=>"ZIPLINE TOUR",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>2
+
+				],
+				[
+					"title"=>"KITE SURFING",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>3
+
+				],
+				
+			),
+		],
+		[
+
+			'cont'  => array(
+				[
+					"title"=>"WOODWARD",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>4
+
+				],
+				[
+					"title"=>"SPORT FISHING",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>5
+
+				],
+				[
+					"title"=>"BUNGEE JUMP",
+					"cont"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+					"num"=>6
+
+				],
+			),
+		]
+	),
+	
+ );
+
+$information=array(
+
+	'information'=> array(
+		[
+			'title'=>'Get ready for the adventure!',
+			'cont'=>'<p class="pb-5">If you haven\'t been at the Riviera Maya  you are definitely losing out, check out all the amazing times your are missing by clicking the video on the left.</p>',
+			'image' => array(
+				"img/tour.png","The best tours"
+			),
+
+			'buttons' => array(
+				"#","LEARN MORE"	,
+			 )
+		],
+		[
+			'title'=>'Get in line for the fun!',
+			'cont'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus faucibus magna id malesuada. Ut dictum ex odio, ut viverra metus semper nec. Suspendisse aliquet enim a velit egestas pharetra. Maecenas venenatis libero nec tellus suscipit, eget luctus lectus finibus. </p>
+
+<p>Curabitur ac tortor eget justo rhoncus porttitor. Vivamus eu libero ullamcorper ipsum euismod efficitur ac eget arcu. In tincidunt erat ut facilisis aliquet. Pellentesque sollicitudin sit amet tellus a facilisis. Curabitur felis ligula, euismod quis purus non, fermentum varius orci. Nam tristique placerat tempus. Nam auctor augue id lectus viverra, eget venenatis nisi porttitor. Proin neque erat, rutrum id neque ac, dapibus sodales est.</p>',
+			'image' => array(
+				"/img/skater.png","The best tours"
+			 ),
+
+		],
+	),
+	
+
+);
+
+
+$contact = array(
+
+	'cont' =>  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus faucibus magna id malesuada. Ut dictum ex odio, ut viverra metus semper nec. Suspendisse aliquet enim a velit egestas pharetra. Maecenas venenatis libero nec tellus suscipit, eget luctus lectus finibus. ',
+	'cont2' =>'Curabitur ac tortor eget justo rhoncus porttitor. Vivamus eu libero ullamcorper ipsum euismod efficitur ac eget arcu. In tincidunt erat ut facilisis aliquet. Pellentesque sollicitudin sit amet tellus a facilisis. Curabitur felis ligula, euismod quis purus non, fermentum varius orci. Nam tristique placerat tempus. Nam auctor augue id lectus viverra, eget venenatis nisi porttitor. Proin neque erat, rutrum id neque ac, dapibus sodales est.',
+
+	'title' =>'CONTACT INFO:',
+	'phone' =>'PHONE:  227 555 0100',
+	'email' =>'E-MAIL:  contact@extremerivieramaya.com',
+	'adress' =>'ADRESS:  Cancún, México.',
+
+);
+
+ ?>
--- index.php
+++ index.php
@@ -5,7 +5,11 @@ require 'array-ingles.php';
 
 $params = array(
 	'title' => 'Coapeñitos',
-	'keywords' => 'Taco Coapeñitos Cancun, Los mejores tacos de Cancun, Tacos Cancun, Taco Cancun, Donde comer tacos en cancun, tacos al pastor cancun, tacos baratos, tacos saludables, tacos vegetarianos.'
+	'keywords' => 'Taco Coapeñitos Cancun, Los mejores tacos de Cancun, Tacos Cancun, Taco Cancun, Donde comer tacos en cancun, tacos al pastor cancun, tacos baratos, tacos saludables, tacos vegetarianos.',
+	'service'=>$services,
+	'op'=>$op,
+	'inf'=>$information,
+	'contact'=>$contact
 );
 ##Hola subisssss
 $template = $twig->load('main.twig');
